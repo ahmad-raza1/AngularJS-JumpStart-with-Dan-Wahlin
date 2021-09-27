9195e60db4c5acd0d36358782acc6a0f71fd3704
AHMEDRAZA\AhmedRaza <ahmad.raza@vicenna.com>
2021-09-27 12:33:56 +0500

    Another option for adding a controller, register it in the app.js first, then use it

--- app/controllers/customersController.js
+++ app/controllers/customersController.js
@@ -1,11 +1,13 @@
-var app = angular.module('myApp', []);
-app.controller('CustomersController', function ($scope) {
-  $scope.sortBy = 'name';
-  $scope.reverse = false;
+(function() {
+  angular.module('customersApp')
+    .controller('CustomersController', function ($scope) {
+    $scope.sortBy = 'name';
+    $scope.reverse = false;
 
-  $scope.customers = [{joined: '2000-12-02', name: 'John', city: 'Chandler', orderTotal: '10'}, {joined: '2010-10-22', name: 'Ross', city: 'Berlin', orderTotal: '15'}, {joined: '2010-02-15', name: 'Pete', city: 'Oslo', orderTotal: '12'}];
-  $scope.doSort = function(propName) {
-    $scope.sortBy = propName;
-    $scope.reverse = !$scope.reverse;
-  };
-});
+    $scope.customers = [{joined: '2000-12-02', name: 'John', city: 'Chandler', orderTotal: '10'}, {joined: '2010-10-22', name: 'Ross', city: 'Berlin', orderTotal: '15'}, {joined: '2010-02-15', name: 'Pete', city: 'Oslo', orderTotal: '12'}];
+    $scope.doSort = function(propName) {
+      $scope.sortBy = propName;
+      $scope.reverse = !$scope.reverse;
+    };
+  });
+}());
