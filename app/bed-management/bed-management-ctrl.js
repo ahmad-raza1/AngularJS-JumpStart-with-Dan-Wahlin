4ad8811322b0a85efc39385bf237ea6508101329
ahmad-raza-vicenna <ahmad.raza@vicenna.com>
2022-03-06 17:20:49 +0500

    Added metrics on top of the page

--- app/bed-management/bed-management-ctrl.js
+++ app/bed-management/bed-management-ctrl.js
@@ -1,7 +1,22 @@
 'use-strict';
 (function () {
     class BedManagementController {
-        constructor($scope) { }
+        constructor($scope) {
+
+            var vm = $scope;
+            vm.metricsTemplateID = "metrics.tmp.html";
+            vm.viewBy = "viewByWard";
+
+            vm.metrics = [
+                "one", "two", "three", "four"
+            ];
+
+            vm.goto = function (viewType) {
+                vm.viewBy = viewType;
+            };
+
+
+        }
     }
 
     BedManagementController.$inject = ['$scope'];
