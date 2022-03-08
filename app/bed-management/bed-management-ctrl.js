8f2c96ff390c32e2939af3bcbb31f0082eed894a
ahmad-raza-vicenna <ahmad.raza@vicenna.com>
2022-03-08 17:42:17 +0500

    Grid customaziation and responsiveness

--- app/bed-management/bed-management-ctrl.js
+++ app/bed-management/bed-management-ctrl.js
@@ -10,14 +10,29 @@
                 vm.viewBy = viewType;
             };
 
+            vm.statusColorCodes = [
+                { status: "Occupied", colorCode: "#4CAF50" },
+                { status: "Available", colorCode: "#607D8B" },
+                { status: "Discharge Advised", colorCode: "#9C27B0" },
+                { status: "Reserved", colorCode: "#0091EA" },
+                { status: "Under Preaparation", colorCode: "#FF9800" },
+                { status: "Blocked", colorCode: "#F44336" },
+            ];
+
+            vm.getStatusColorCode = function (status) {
+                let statusColorCode = vm.statusColorCodes.find(x => x.status == status);
+                let colorStyle = statusColorCode != undefined ? `{'color': '${statusColorCode.colorCode}'}` : "{'color': 'inherit'}";
+                return colorStyle;
+            };
+
             vm.wards = [
                 {
                     wardID: 1, ward: "Ward A", rooms: [
                         {
                             roomID: 1, room: "Semi Private", wardID: 1, beds: [
-                                { bedId: 1, bed: "109-A", patient: "Sarah", status: "Occupied", roomID: 1 },
-                                { bedId: 2, bed: "109-A", patient: "David Ross", status: "Available", roomID: 1 },
-                                { bedId: 3, bed: "109-A", patient: "Rita Fiona", status: "Discharge Advised", roomID: 1 }
+                                { bedID: 1, bed: "109-A", patient: "Sarah", status: "Occupied", roomID: 1 },
+                                { bedID: 2, bed: "109-A", patient: "David Ross", status: "Available", roomID: 1 },
+                                { bedID: 3, bed: "109-A", patient: "Rita Fiona", status: "Discharge Advised", roomID: 1 }
                             ]
                         },
                         { roomID: 2, room: "Deluxe", wardID: 1 }
@@ -36,11 +51,14 @@
 
             vm.mainGridOptions = {
                 dataSource: vm.wards,
-                columns: [{
-                    field: "ward",
-                    title: "Ward"
-                }
-                ]
+                columns: [
+                    { field: "ward", title: "Ward" }
+                ],
+                attributes: {
+                    class: "font-bold",
+                    style: ""
+                },
+                noRecords: true
             };
 
             vm.detailGridOptions = function (dataItem) {
@@ -49,7 +67,12 @@
                     dataSource: vm.rooms.filter(r => r.wardID == dataItem.wardID),
                     columns: [
                         { field: "room", title: "Room" }
-                    ]
+                    ],
+                    attributes: {
+                        class: "font-bold",
+                        style: ""
+                    },
+                    noRecords: true
                 };
             };
 
@@ -58,14 +81,18 @@
                 return {
                     dataSource: vm.beds.filter(b => b.roomID == dataItem.roomID),
                     columns: [
-                        { field: "bed", title: "Bed", width: "190px" },
-                        { field: "patient", title: "Patient", width: "190px" },
-                        { field: "status", title: "Status", width: "190px" },
+                        { field: "bed", title: "Bed", width: "320px" },
+                        { field: "patient", title: "Patient", width: "320px" },
                         {
-                            field: "",
-                            template: "<md-icon class='material-icons'>more_vert</md-icon>"
+                            field: "status", title: "Status", width: "320px",
+                            attributes: {
+                                class: "font-bold",
+                                style: ""
+                            }
                         }
-                    ]
+                    ],
+                    rowTemplate: kendo.template($("#rowTemplate").html()),
+                    noRecords: true
                 };
             };
 
